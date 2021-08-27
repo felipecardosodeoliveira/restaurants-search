fefdeddf42890bc32d86cc6c56d44c40b6860608
lipeoliveira <felipecardosodeoliveira02@gmail.com>
2021-08-26 22:32:36 -0300

    iniciando projeto

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,4 @@
+
 const theme = {
   colors: {
     primary: '#6200ee',
@@ -6,7 +7,7 @@ const theme = {
     success: '#135E01',
   },
   fonts: {
-    fontFamily: 'Roboto, sans-serif',
+    regular: 'Roboto, sans-serif',
   },
 };
 
