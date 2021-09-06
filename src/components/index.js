a274f2e7b1342960e76478480eb4656e44ed9858
Felipe Cardoso de Oliveira <63833005+felipecardosodeoliveira@users.noreply.github.com>
2021-09-06 11:24:17 -0300

    Delete src/components directory

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Card } from './ImageCard'
-export { default as RestaurantCard } from './RestaurantCard';
-export { default as Modal } from "./Modal";
-export { default as Map } from './Mapa';
